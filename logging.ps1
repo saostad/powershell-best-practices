5967155195c5ab3d3ff9df497b3f5114d149d62f
saostad <16025625+saostad@users.noreply.github.com>
2021-05-18 10:30:54 -0400

    Update logging.ps1

--- logging.ps1
+++ logging.ps1
@@ -1,8 +1,11 @@
+[string]$logFileName = Get-Date -Format HH_mm_ss_fff
+Write-Output "Writing logs to file $logFileName.txt"
+
 Function Log {
-    param(
-        [Parameter(Mandatory=$true)][String]$msg
-    )
-    
-    Add-Content log.txt $msg
-    Write-Output $msg
+  param(
+    [Parameter(Mandatory = $true)][String]$msg
+  )
+  
+  Add-Content "$logFileName.txt" $msg
+  Write-Output $msg
 }
