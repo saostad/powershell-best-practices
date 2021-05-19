ed23e545ff639ae7d794a8e47d4b14f8c636939c
saostad <16025625+saostad@users.noreply.github.com>
2021-05-18 20:14:58 -0400

    Create objects.ps1

---
+++ objects.ps1
@@ -0,0 +1,11 @@
+## Define the hashtable
+$CarHashtable = @{
+	Brand      = 'Ford'
+	Style      = 'Truck'
+	Model      = 'F-150'
+	Color      = 'Red'
+	Drivetrain = '4x4'
+}
+
+## Create an object
+$CarObject = [PsCustomObject]$CarHashTable
