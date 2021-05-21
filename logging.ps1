9e78355d65b00ade890cd801b21c26fd3b6bf60a
saostad <16025625+saostad@users.noreply.github.com>
2021-05-21 14:45:57 -0400

    Update logging.ps1

--- logging.ps1
+++ logging.ps1
@@ -1,11 +1,38 @@
-[string]$logFileName = Get-Date -Format HH_mm_ss_fff
-Write-Output "Writing logs to file $logFileName.txt"
+[string]$logFileName = Get-Date -Format yyyy_MM_HH_mm_ss_fff
+Write-Output "Writing logs to file $logFileName.log"
 
 Function Log {
   param(
-    [Parameter(Mandatory = $true)][String]$msg
+    [Parameter(Mandatory = $true)][String]$msg,
+    [Parameter(Mandatory = $false)][String]$level,
+    [Parameter(Mandatory = $false)][System.ConsoleColor]$ForegroundColor,
+    [Parameter(Mandatory = $false)][System.ConsoleColor]$BackgroundColor
   )
   
-  Add-Content "$logFileName.txt" $msg
-  Write-Output $msg
+  $time = Get-Date -Format HH:mm:ss.fff
+  Add-Content "logs\\$logFileName.log" "$time $msg"
+  if ($ForegroundColor -and $BackgroundColor) {
+    Write-Host "$time $msg" -ForegroundColor $ForegroundColor -BackgroundColor $BackgroundColor
+  }
+  elseif ($ForegroundColor) {
+    Write-Host "$time $msg" -ForegroundColor $ForegroundColor
+  }
+  elseif ($BackgroundColor) {
+    Write-Host "$time $msg" -BackgroundColor $BackgroundColor
+  }
+  elseif ($level.ToLower() -eq "warn") {
+    Write-Warning "$time $msg" 
+  }
+  elseif ($level.ToLower() -eq "error") {
+    Write-Error "$time $msg" 
+  }
+  elseif ($level.ToLower() -eq "success") {
+    Write-Host "$time $msg" -ForegroundColor White -BackgroundColor DarkBlue
+  }
+  elseif ($level.ToLower() -eq "progress") {
+    Write-Host "$time $msg" -ForegroundColor White -BackgroundColor DarkGray
+  }
+  else {
+    Write-Host "$time $msg"
+  }
 }
