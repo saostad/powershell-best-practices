98211005c8f7875847be48cbeabf0ebe67089796
saostad <16025625+saostad@users.noreply.github.com>
2021-05-26 20:56:13 -0400

    Update sql-server-connection.ps1

--- sql-server-connection.ps1
+++ sql-server-connection.ps1
@@ -5,20 +5,30 @@ function CreateDbConnection {
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$dbName,
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$hostName
   )
-  [System.Data.SqlClient.SqlConnection]$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-  $SqlConnection.ConnectionString = "Server = $hostName; Database = $dbName; Integrated Security = False; TimeOut = 100000; User ID = $uid; Password = $password"
+
+  try {
+    [System.Data.SqlClient.SqlConnection]$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+
+    $connectionString = "Server = $hostName; Database = $dbName; Integrated Security = False; TimeOut = 100000; User ID = $uid; Password = $password"
+
+    $SqlConnection.ConnectionString = $connectionString
   
-  if ($SqlConnection.State -eq "close") {
-    log -level "progress" -msg "Connecting to DB $dbName host $hostName..."
-    $SqlConnection.Open()
-    if ($SqlConnection.State -eq "open") {
-      log -level "success" -msg "Connected to DB $dbName Successfully!"
-      return $SqlConnection      
+    if ($SqlConnection.State -eq "close") {
+      log -level "progress" -msg "Connecting to DB $dbName on host $hostName..."
+      $SqlConnection.Open()
+      if ($SqlConnection.State -eq "open") {
+        log -level "success" -msg "Connected to DB $dbName on host $hostName Successfully!"
+        return $SqlConnection      
+      }
+      else {
+        log -level "error" -msg "Connection to DB $dbName on host $hostName failed!"
+        log -msg "Retrying to connect to DB $dbName on host $hostName..."
+        return CreateDbConnection -uid $uid -password "$($password)" -dbName $dbName -hostName $hostName     
+      }
     }
-    else {
-      log -level "error" -msg "Connection to DB $dbName failed!"
-      log -msg "Retrying to connect to DB $dbName..."
-      return CreateDbConnection -uid $uid -password $password -dbName $dbName -hostName $hostName     
-    }
+  }
+  catch [System.Exception] {
+    $_.Exception | Write-Output
+    Log -level "error" -msg "error while connecting to DB $dbName on host $hostName." 
   }
 } # CreateConnectionToDB
