7c03b83f07811d43595bb7f80b2957824a538209
Saeid Ostad <sostad@kajimausa.com>
2021-06-14 17:25:36 -0400

    enhance: sql connection code sample

--- code-samples/sql-server-connection.ps1
+++ code-samples/sql-server-connection.ps1
@@ -32,3 +32,22 @@ function CreateDbConnection {
     Log -level "error" -msg "error while connecting to DB $dbName on host $hostName." 
   }
 } # CreateConnectionToDB
+
+
+
+# How to use example
+[string]$collectionsQuery = "select * from TABLE_NAME"
+[string]$sccmDbHost = "*****"
+[string]$sccmDbName = "*****"
+[System.Data.SqlClient.SqlConnection]$sccmCon = CreateDbConnection -uid $SccmDbUserName -password $SccmDbPassword -dbName $sccmDbName -hostName $sccmDbHost
+[System.Data.SqlClient.SqlCommand]$collectionsCmd = New-Object System.Data.SqlClient.SqlCommand
+
+$collectionsCmd.CommandTimeout = 960
+$collectionsCmd.Connection = $sccmCon
+$collectionsCmd.CommandText = $collectionsQuery
+[System.Data.SqlClient.SqlDataReader]$sccmDataReader = $collectionsCmd.ExecuteReader()
+
+$dataTable = New-Object System.Data.DataTable 
+$dataTable.Load($sccmDataReader)
+$numRows = $dataTable.Rows.Count
+log -level "success" -msg "$("{0:N0}" -f $numRows) rows downloaded." 
